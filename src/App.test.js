416144898bf0b89c194fdd1228c04a5a8d97400e
Densitty <dennis.brough@yandex.com>
2022-01-31 07:12:52 -0600

    writing my first test using a TDD approach

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("button has correct initial color", () => {
+  // 1. render the component we are testing for
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // 2. find the element we are interested in, name is the text on d ele
+  const colorBtn = screen.getByRole("button", { name: "Change to Blue" });
+  // 3. assertion test for the condition
+  expect(colorBtn).toHaveStyle({ backgroundColor: "red" });
 });
+
+// test("button has correct initial text", () => {});
+
+// test("button turns blue when clicked", () => {});
